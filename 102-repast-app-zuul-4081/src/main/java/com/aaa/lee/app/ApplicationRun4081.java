1782216ff7d2f2b0da361af9ca14129e48ad6fde
an17630811559 <52552813+an17630811559@users.noreply.github.com>
2019-12-18 18:39:59 +0800

    代码初始化

---
+++ 102-repast-app-zuul-4081/src/main/java/com/aaa/lee/app/ApplicationRun4081.java
@@ -0,0 +1,12 @@
+package com.aaa.lee.app;
+
+/**
+ * @author AAA QY102 awb
+ * @description .
+ * @date create in 18:38 2019/12/18
+ */
+public class ApplicationRun4081 {
+    public static void main(String[] args) {
+
+    }
+}
