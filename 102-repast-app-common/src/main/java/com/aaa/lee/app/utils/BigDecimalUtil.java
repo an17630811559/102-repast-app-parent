32ddb8df95e4e9eba4861679b4626495eb416816
an17630811559 <52552813+an17630811559@users.noreply.github.com>
2019-12-20 10:26:04 +0800

    新增工具类 BigDecimalUtil 用户类型转换

---
+++ 102-repast-app-common/src/main/java/com/aaa/lee/app/utils/BigDecimalUtil.java
@@ -0,0 +1,38 @@
+package com.aaa.lee.app.utils;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+/**
+ * @author AAA QY102 awb
+ * @description 类型转换工具类
+ * @date create in 10:24 2019/12/20
+ */
+public class BigDecimalUtil {
+
+
+
+    /**
+     * Object转BigDecimal类型
+     *
+     * @param value 要转的object类型
+     * @return 转成的BigDecimal类型数据
+     */
+    public static BigDecimal getBigDecimal(Object value) {
+        BigDecimal ret = null;
+        if (value != null) {
+            if (value instanceof BigDecimal) {
+                ret = (BigDecimal) value;
+            } else if (value instanceof String) {
+                ret = new BigDecimal((String) value);
+            } else if (value instanceof BigInteger) {
+                ret = new BigDecimal((BigInteger) value);
+            } else if (value instanceof Number) {
+                ret = new BigDecimal(((Number) value).doubleValue());
+            } else {
+                throw new ClassCastException("Not possible to coerce [" + value + "] from class " + value.getClass() + " into a BigDecimal.");
+            }
+        }
+        return ret;
+    }
+}
