ca9b685eb6ed95074e5c7048cb4057f8f87cab00
an17630811559 <52552813+an17630811559@users.noreply.github.com>
2019-12-20 13:43:45 +0800

    更新mapper

--- 102-repast-app-management/102-repast-app-mapper/src/main/java/com/aaa/lee/app/mapper/OmsCartItemMapper.java
+++ 102-repast-app-management/102-repast-app-mapper/src/main/java/com/aaa/lee/app/mapper/OmsCartItemMapper.java
@@ -4,12 +4,6 @@ import com.aaa.lee.app.model.OmsCartItem;
 import org.springframework.stereotype.Repository;
 import tk.mybatis.mapper.common.Mapper;
 
-import java.util.Map;
-
 @Repository
 public interface OmsCartItemMapper extends Mapper<OmsCartItem> {
-
-
-     Integer selectSku(OmsCartItem omsCartItem);
-
 }
--- 102-repast-app-management/102-repast-app-mapper/src/main/java/com/aaa/lee/app/mapper/OmsOrderMapper.java
+++ 102-repast-app-management/102-repast-app-mapper/src/main/java/com/aaa/lee/app/mapper/OmsOrderMapper.java
@@ -1,7 +1,11 @@
 package com.aaa.lee.app.mapper;
 
 import com.aaa.lee.app.model.OmsOrder;
+import org.springframework.stereotype.Repository;
 import tk.mybatis.mapper.common.Mapper;
 
+@Repository
 public interface OmsOrderMapper extends Mapper<OmsOrder> {
+
+
 }
