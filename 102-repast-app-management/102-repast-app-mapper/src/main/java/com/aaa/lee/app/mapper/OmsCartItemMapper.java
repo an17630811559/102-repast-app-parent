d2616b608f49a4772e055649965e138a94bab478
YLJ519 <58689251+YLJ519@users.noreply.github.com>
2020-01-08 17:33:52 +0800

    添加购物车提交

--- 102-repast-app-management/102-repast-app-mapper/src/main/java/com/aaa/lee/app/mapper/OmsCartItemMapper.java
+++ 102-repast-app-management/102-repast-app-mapper/src/main/java/com/aaa/lee/app/mapper/OmsCartItemMapper.java
@@ -6,4 +6,12 @@ import tk.mybatis.mapper.common.Mapper;
 
 @Repository
 public interface OmsCartItemMapper extends Mapper<OmsCartItem> {
+
+    /**
+     * 修改购物车的数量
+     * @param omsCartItem
+     * @return
+     */
+    Integer  updateCartQuantity(OmsCartItem omsCartItem);
+
 }
